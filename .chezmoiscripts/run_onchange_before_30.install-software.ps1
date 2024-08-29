39628a0b376e6ed872ef6511d1964a066f840cc6
Alexey Zudenkov <drunaar@gmail.com>
2024-08-29 12:39:51 +0200

    feat: install terminal and shells

---
+++ .chezmoiscripts/run_onchange_before_30.install-software.ps1
@@ -0,0 +1,27 @@
+#!/usr/bin/env powershell
+
+$softwareToInstall = @{
+  scoop = (
+    # -- terminal & shells
+    'extras/windows-terminal',
+    'main/pwsh',
+    'main/nu',
+    'main/oh-my-posh'
+  )
+  winget = @()
+}
+
+$softwareToInstall.winget `
+| Where-Object { winget list --exact --id $_ --accept-source-agreements } `
+| ForEach-Object { winget install --exact --id $_ --interactive --accept-source-agreements --accept-package-agreements }
+
+$currentState = scoop export | ConvertFrom-Json
+
+$softwareToInstall.scoop `
+| ForEach-Object { ($_ -split '/')[0].ToLower() } | Select-Object -Unique `
+| Where-Object { $_ -inotin $($currentState.buckets | ForEach-Object Name) } `
+| ForEach-Object { scoop bucket add $_ }
+
+$softwareToInstall.scoop `
+| Where-Object { $_ -inotin $($currentState.apps | ForEach-Object { "$($_.Source)/$($_.Name)" } ) } `
+| ForEach-Object { scoop install $_ }
